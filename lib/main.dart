59640ba9a9458832fe3aab0259e76875316fdd75
icchigoo <icchigoo12@gmail.com>
2023-03-08 19:55:32 +0545

    vehicle,create_ride,rides,search_ride UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,13 @@
+// ignore_for_file: deprecated_member_use
+
+import 'package:carpooling/providers/theme.dart';
+import 'package:carpooling/screens/add_vehicle.dart';
+import 'package:carpooling/screens/create_ride1.dart';
+import 'package:carpooling/screens/create_ride2.dart';
+import 'package:carpooling/screens/rides.dart';
+import 'package:carpooling/screens/search_ride.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(MyApp());
@@ -6,25 +15,57 @@ void main() {
 
 class MyApp extends StatelessWidget {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Carpooling',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(),
-    );
-  }
+  Widget build(BuildContext context) => ChangeNotifierProvider(
+        create: (context) => ThemeProvider(),
+        builder: (context, _) {
+          final themeProvider = Provider.of<ThemeProvider>(context);
+
+          return MaterialApp(
+            debugShowCheckedModeBanner: false,
+            title: "Carpooling",
+            themeMode: themeProvider.themeMode,
+            theme: MyThemes.lightTheme,
+            darkTheme: ThemeData.dark(),
+            // MyThemes.darkTheme,
+            home: SearchRide(),
+          );
+        },
+      );
 }
 
 class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text(
-          'Login',
-        ),
+      body: Column(
+        children: [
+          Center(
+            child: Text(
+              'Login sv  dvsdc',
+            ),
+          ),
+          TextButton(
+              onPressed: () {
+                final themeProvider =
+                    Provider.of<ThemeProvider>(context, listen: false);
+                themeProvider.toggleTheme(true);
+              },
+              child: Text(
+                "dark",
+                style: TextStyle(color: Theme.of(context).accentColor),
+              )),
+          TextButton(
+            onPressed: () {
+              final themeProvider =
+                  Provider.of<ThemeProvider>(context, listen: false);
+              themeProvider.toggleTheme(false);
+            },
+            child: Text(
+              "light",
+              style: TextStyle(color: Theme.of(context).accentColor),
+            ),
+          )
+        ],
       ),
     );
   }
